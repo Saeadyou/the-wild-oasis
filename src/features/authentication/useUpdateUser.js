92d075fffcb4b6c9e2c96750fc5a1db7cb1f92e4
saeadyou <saeadyou@gmail.com>
2023-10-11 14:43:18 +0200

    build update user data and password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const QueryClient = useQueryClient();
+
+  const { isLoading: isupdating, mutate: updateUser } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("User account successfully updated");
+      //   QueryClient.setQueryData("user",user)
+
+      QueryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isupdating, updateUser };
+}
